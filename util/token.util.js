a594f161abd1852738d0a64fab6589b198acc740
hidnarola <hda@narola.email>
2020-05-18 15:39:43 +0530

    fixing bug

--- util/token.util.js
+++ util/token.util.js
@@ -1,21 +1,29 @@
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
 
-var createToken = function (auth) {
-    return jwt.sign({
-        id: auth.id
-    }, 'my-secret',
-        {
-            expiresIn: 60 * 120
-        });
+var createToken = function(auth) {
+  return jwt.sign(
+    {
+      id: auth.id
+    },
+    "my-secret",
+    {
+      expiresIn: 60 * 120
+    }
+  );
 };
 
 module.exports = {
-    generateToken: function (req, res, next) {
-        req.token = createToken(req.auth);
-        return next();
-    },
-    sendToken: function (req, res) {
-        res.setHeader('x-auth-token', req.token);
-        return res.status(200).send(JSON.stringify(req.user));
-    }
+  generateToken: function(req, res, next) {
+    req.token = createToken(req.auth);
+    return next();
+  },
+  sendToken: function(req, res) {
+    res.setHeader("x-auth-token", req.token);
+    let requestData = {
+      data: req.user,
+      token: req.token,
+      status: 1
+    };
+    return res.status(200).send(requestData);
+  }
 };
