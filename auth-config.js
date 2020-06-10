d98b530d5ee77e3256f59246979bf4b3793f7bf1
hidnarola <hda@narola.email>
2020-06-10 18:24:24 +0530

    facebook login changes

--- auth-config.js
+++ auth-config.js
@@ -2,8 +2,8 @@ module.exports = {
   facebookAuth: {
     clientID: "2831009733683828",
     clientSecret: "8307558fd96c78238622e40eb273df74",
-    // callbackURL: "http://localhost:3000/auth/facebook/callback",
-    callbackURL: "https://seekrep-backend.herokuapp.com/auth/facebook/callback",
+    callbackURL: "http://localhost:3000/auth/facebook/callback",
+    // callbackURL: "https://seekrep-backend.herokuapp.com/auth/facebook/callback",
     scope: ["email"]
     // profileURL: "https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email"
   },
